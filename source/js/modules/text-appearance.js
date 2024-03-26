907f3b3d77a41e34c23ecb1adc4f16a849bb3dfc
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-03-26 13:46:57 +0300

    flamingo and suicase anims donr

--- source/js/modules/text-appearance.js
+++ source/js/modules/text-appearance.js
@@ -3,8 +3,18 @@ import textAppearanceAnimation from "../text-appearance-animation.js";
 function textAppearance () {
     const titleMain = document.querySelector('.intro__title');
     const introDate = document.querySelector('.intro__date');
+    const sliderItemTitle = document.querySelector('.slider__item-title');
+    const prizesTitle = document.querySelector('.prizes__title');
+    const rulesTitle = document.querySelector('.rules__title');
+    const gameTitle = document.querySelector('.game__title');
+
+
     textAppearanceAnimation(titleMain);
     textAppearanceAnimation(introDate);
+    textAppearanceAnimation(sliderItemTitle);
+    textAppearanceAnimation(prizesTitle);
+    textAppearanceAnimation(rulesTitle);
+    textAppearanceAnimation(gameTitle);
 }
 
 export default textAppearance;
